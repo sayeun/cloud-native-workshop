5e4b7e82cbb3e3f77c56df81463108b5d42b5b52
Sally Yeung <Sally.Yeung@sas.com>
2017-03-29 15:34:41 -0400

    initial commit 3

---
+++ 12f_app/src/main/java/com/github/sayeun/app/LogoutResource.java
@@ -0,0 +1,19 @@
+package com.github.sayeun.app;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LogoutResource {
+
+    @RequestMapping(value = "/logout", method = RequestMethod.GET)
+    public String logout(HttpSession session) {
+
+        session.invalidate();
+        return "Logout done!";
+    }
+
+}
